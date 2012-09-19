5958cdbc6fda7803d1f708650219950b71c716b9
Garren Smith <garren.smith@gmail.com>
2012-09-19 12:50:52 +0200

    logo

--- grunt.js
+++ grunt.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
     watch: {
              //files: '<config:lint.files>',
              //tasks: 'lint test'
-             files: ['*.less', '*.js'],
+             files: ['*.html', '*.less', '*.js'],
              tasks: 'less yowzer'
            },
 
